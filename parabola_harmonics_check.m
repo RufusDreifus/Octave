a3ebbba0f05017281ce653b158c01facf8b8900a
RufusDreifus <nikolay@nbukharin.com>
2023-11-16 17:54:04 -0700

    minor cosmetic changes

--- parabola_harmonics_check.m
+++ parabola_harmonics_check.m
@@ -1,21 +1,27 @@
+%Approximation of parabola with trigonometric series. Written in Octave.
+
 t = linspace(-3, 3, 100000);
 
 y = t.^2;
-
 f = @(n) (-1)^n * cos(n * t) ./ n.^2;
 
 % Initialize y1 as zeros
 y1 = zeros(size(t));
 
 % Loop to accumulate values
-for n = 1:50
+for n = 1:50 %number of harmonics
     y1 = y1 + f(n);
 end
+
+%plotting functions
 
 plot(t, y, t, pi^2/3 + 4.*y1);
 
 set(gca, 'XAxisLocation', 'origin');
 set(gca, 'YAxisLocation', 'origin');
+
+yticks(0:0.5:9)
+xticks(-3:0.5:3)
 
 legend('t^2', 'Sum of harmonics');
 
